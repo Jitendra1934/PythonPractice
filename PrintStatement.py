b296de3d58f313ca05d0ed67ed324fb751429387
jitendra1934 <punnamjitendra@gmail.com>
2025-11-11 13:00:26 +0530

    String Concat

---
+++ PrintStatement.py
@@ -0,0 +1,3 @@
+print(1)
+print(200)
+print("200 is great number")
---
+++ StringConcatination.py
@@ -0,0 +1,6 @@
+print("My favorite color is blue")
+#Concat with str
+print("My favorite color is blue and number "+str(24)+" is my fav")
+#Concat with f
+print(f"My favorite color is blue and number {24} is my fav")
+
