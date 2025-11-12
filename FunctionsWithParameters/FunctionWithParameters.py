5c1d8d582ad8ed70dd85d9285b2bd5576062b304
jitendra1934 <punnamjitendra@gmail.com>
2025-11-12 12:21:58 +0530

    Functions with Parameters(arguments)

---
+++ FunctionsWithParameters/FunctionWithParameters.py
@@ -0,0 +1,14 @@
+message = "succesfull"
+
+def add(a,b, CustomMessage):
+    print(f"{a+b} {CustomMessage}")
+    print(message)
+
+add(2,2, 'is successfully added')
+
+def mul(a,b, CustomMessage):
+    print(f"{a*b} {CustomMessage}")
+    print(message)
+
+mul(2,2, 'is successfully multiplied')
+
---
+++ FunctionsWithParameters/Functions.py
@@ -0,0 +1,13 @@
+a=2
+b=2
+message ="succesfull"
+
+def add():
+    print(f"{a+b} {message}")
+
+add()
+
+def subtraction():
+    print(f"{a-b} {message}")
+
+subtraction()
