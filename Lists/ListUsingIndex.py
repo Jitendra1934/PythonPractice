53a85ec97588d23f995a0a6313603897d9fbaa98
jitendra1934 <punnamjitendra@gmail.com>
2025-11-18 12:50:10 +0530

    List with the For loops

---
+++ Lists/ListUsingIndex.py
@@ -0,0 +1,6 @@
+def listFun():
+    li = [1,"2","text",99.99]
+    for i in range(len(li)):
+        print(f"{i} is the index number")
+        print(f"{li[i]} is the value\n")
+listFun()
---
+++ Lists/ListWithBasicForLoop.py
@@ -0,0 +1,6 @@
+def listFun():
+    li = [1, "2", '3', "text", 99.99]
+    for i in li:
+        print(i)
+
+listFun()
