8bb896a1d191477d1082bd3240224473f6904664
jitendra1934 <punnamjitendra@gmail.com>
2025-11-25 14:00:34 +0530

    list and set methods

---
+++ Lists&Sets/SetMethods.py
@@ -0,0 +1,24 @@
+s = {1,2,3,4,4,5,6,7,7,8}
+
+print(s)
+
+s.add(9)
+
+print(s)
+
+s.update([10,11,11])
+
+print(s)
+
+s.remove(3)
+
+print(s)
+
+s.discard(10)
+
+print(s)
+
+s.pop()
+
+print(s)
+
---
+++ Lists&Sets/addElemenmtsInSet.py
@@ -0,0 +1,13 @@
+s={'jan', 'feb', 'mar'}
+print(s)
+
+for i in s:
+    print(i)
+
+my_list = list(s)
+
+my_list.sort()
+
+print(my_list)
+
+print(my_list[2])
