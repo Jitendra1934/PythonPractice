6f0b42ee30a3f7d32ac47e223ca9bf3471fdd9f5
jitendra1934 <punnamjitendra@gmail.com>
2025-12-08 11:01:39 +0530

    class and object

---
+++ Opps/ClassAndObject.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, name, age, role):
+        self.name = name
+        self.age = age
+        self.role = role
+
+    def get_Details(self):
+        print(f"Name: {self.name}, Age: {self.age}, Role: {self.role}")
+
+user1 = User("jitendra", 20, "Tester")
+user1.get_Details()
+
+user2 = User("Ajay", 21, "Manual Tester")
+user2.get_Details()
+
+user3 = User("jitendra", 20, "Automation Tester")
+user3.get_Details()
